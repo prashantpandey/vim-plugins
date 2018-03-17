e91b91d871cb387eacee54a44a06709a1e217f04
Prashant Pandey <prashantpandey@users.noreply.github.com>
2018-03-17 09:18:05 -0400

    More repos added.

--- bootstrap-repo.sh
+++ bootstrap-repo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get -y install vim git openssh-client openssh-server libssl-dev cscope ctags exuberant-ctags build-essential cmake libboost-all-dev libgnome2-bin tmux
+sudo apt-get -y install vim git openssh-client openssh-server libssl-dev cscope ctags exuberant-ctags build-essential cmake libboost-all-dev libgnome2-bin tmux xclip
 mkdir code
 mkdir paper
 cd code
@@ -7,11 +7,26 @@ git clone git@github.com:splatlab/kmercounting.git
 git clone git@github.com:splatlab/countingqf.git
 git clone git@github.com:splatlab/KMC.git
 git clone git@github.com:prashantpandey/firehose.git
+git clone git@github.com:splatlab/popcorning.git
+git clone git@github.com:splatlab/debgr.git
+git clone git@github.com:splatlab/coloreddbg.git
+git clone git@github.com:splatlab/cqf.git
+git clone git@github.com:splatlab/mantis.git
+git clone git@github.com:splatlab/squeakr.git
 git clone git@github.com:splatlab/wo-skiplist.git
+git clone git@github.com:combine-lab/rainbowfish.git
+git clone git@github.com:oscarlab/betrfs.git
+git clone git@github.com:oscarlab/betrfs-private.git
+git clone git@github.com:oscarlab/Be-Tree.git
 cd -
 cd paper;
 git clone git@github.com:splatlab/countingqf-paper.git
 git clone git@github.com:splatlab/nsf-16-515-amqs.git
 git clone git@github.com:splatlab/kmercounting-paper.git
+git clone git@github.com:splatlab/efficientselect-paper.git
+git clone git@github.com:combine-lab/mantis_paper.git
+git clone git@github.com:combine-lab/rainbowfish-paper.git
+git clone git@github.com:oscarlab/affine-model.git
+git clone git@github.com:oscarlab/betrfs-papers.git
 cd -
 
